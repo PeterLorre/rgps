c4e7ce94b36544b4dc0da27e0e239b8c39d0fc39
Peter Lorre <me.use.android@gmail.com>
2014-11-10 12:06:55 -0700

    Added top menu

--- header.php
+++ header.php
@@ -48,6 +48,23 @@
 
 			<header class="header" role="banner">
 
+					<div class="top-menu cf">
+						<?php wp_nav_menu(array(
+    					'container' => false,                           // remove nav container
+    					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					'menu' => __( 'The Top Menu', 'rgpstheme' ),  // nav name
+    					'menu_class' => 'nav top-nav last-col cf',               // adding custom nav class
+    					'theme_location' => 'top-nav',                 // where it's located in the theme
+    					'before' => '',                                 // before the menu
+        			'after' => '',                                  // after the menu
+        			'link_before' => '',                            // before each link
+        			'link_after' => '',                             // after each link
+        			'depth' => 0,                                   // limit the depth of the nav
+    					'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
+
+					</div>
+
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
@@ -61,9 +78,9 @@
 						<?php wp_nav_menu(array(
     					'container' => false,                           // remove nav container
     					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					'menu' => __( 'The Main Menu', 'rgpstheme' ),  // nav name
-    					'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					'theme_location' => 'main-nav',                 // where it's located in the theme
+    					'menu' => __( 'The Primary Menu', 'rgpstheme' ),  // nav name
+    					'menu_class' => 'nav primary-nav cf',               // adding custom nav class
+    					'theme_location' => 'primary-nav',                 // where it's located in the theme
     					'before' => '',                                 // before the menu
         			'after' => '',                                  // after the menu
         			'link_before' => '',                            // before each link
