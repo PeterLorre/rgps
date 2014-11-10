215d32f5c679bb3e3c9ba74b65bfca428a936c04
Peter Lorre <me.use.android@gmail.com>
2014-11-10 13:42:19 -0700

    top menu and logo adjustments

--- header.php
+++ header.php
@@ -45,30 +45,30 @@
 	<body <?php body_class(); ?>>
 
 		<div id="container">
+		
+			<div class="top-menu cf">
+				<?php wp_nav_menu(array(
+				'container' => false,                           // remove nav container
+				'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+				'menu' => __( 'The Top Menu', 'rgpstheme' ),  // nav name
+				'menu_class' => 'nav top-nav cf',               // adding custom nav class
+				'theme_location' => 'top-nav',                 // where it's located in the theme
+				'before' => '',                                 // before the menu
+				'after' => '',                                  // after the menu
+				'link_before' => '',                            // before each link
+				'link_after' => '',                             // after each link
+				'depth' => 0,                                   // limit the depth of the nav
+				'fallback_cb' => ''                             // fallback function (if there is one)
+				)); ?>
+
+			</div>
 
 			<header class="header" role="banner">
-
-					<div class="top-menu cf">
-						<?php wp_nav_menu(array(
-    					'container' => false,                           // remove nav container
-    					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					'menu' => __( 'The Top Menu', 'rgpstheme' ),  // nav name
-    					'menu_class' => 'nav top-nav last-col cf',               // adding custom nav class
-    					'theme_location' => 'top-nav',                 // where it's located in the theme
-    					'before' => '',                                 // before the menu
-        			'after' => '',                                  // after the menu
-        			'link_before' => '',                            // before each link
-        			'link_after' => '',                             // after each link
-        			'depth' => 0,                                   // limit the depth of the nav
-    					'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
-
-					</div>
 
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a class="logo" href="<?php echo home_url(); ?>" rel="nofollow"><?php echo bloginfo('name'); ?><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php echo bloginfo('name'); ?>" /></a>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
@@ -82,10 +82,10 @@
     					'menu_class' => 'nav primary-nav cf',               // adding custom nav class
     					'theme_location' => 'primary-nav',                 // where it's located in the theme
     					'before' => '',                                 // before the menu
-        			'after' => '',                                  // after the menu
-        			'link_before' => '',                            // before each link
-        			'link_after' => '',                             // after each link
-        			'depth' => 0,                                   // limit the depth of the nav
+	        			'after' => '',                                  // after the menu
+	        			'link_before' => '',                            // before each link
+	        			'link_after' => '',                             // after each link
+	        			'depth' => 0,                                   // limit the depth of the nav
     					'fallback_cb' => ''                             // fallback function (if there is one)
 						)); ?>
 
