5e3c3883acd198bbca5664ee0c5a95ad42fe810b
Peter Lorre <me.use.android@gmail.com>
2014-11-12 10:31:55 -0700

    removing cpt

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ function rgps_ahoy() {
   load_theme_textdomain( 'rgpstheme', get_template_directory() . '/library/translation' );
 
   // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
-  require_once( 'library/custom-post-type.php' );
+  //require_once( 'library/custom-post-type.php' );
 
   // launching operation cleanup
   add_action( 'init', 'rgps_head_cleanup' );
