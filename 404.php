1e19801c94e0b1a24232ed3507f29adf01bf6f3a
Peter Lorre <me.use.android@gmail.com>
2014-11-13 04:26:11 -0700

    adding 404 page

--- 404.php
+++ 404.php
@@ -1,43 +1,10 @@
-<?php get_header(); ?>
+<?php get_header( '404' ); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-					<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
-
-						<article id="post-not-found" class="hentry cf">
-
-							<header class="article-header">
-
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'rgpstheme' ); ?></h1>
-
-							</header>
-
-							<section class="entry-content">
-
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'rgpstheme' ); ?></p>
-
-							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'rgpstheme' ); ?></p>
-
-							</footer>
-
-						</article>
-
-					</div>
-
-				</div>
-
-			</div>
-
-<?php get_footer(); ?>
+	<section class="not-found">
+		<div>
+			<h1>404</h1>
+			<h2>NOT FOUND</h2>	
+			<button><a href="<?php echo site_url(); ?>">Go Home</a></button>
+			<button><a href="<?php echo site_url(); ?>/blog">Visit The Blog</a></button>
+		</div>
+	</section>
