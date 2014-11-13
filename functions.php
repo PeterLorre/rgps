8c443320c65061900146dc29bf5337ce0d9c19af
Peter Lorre <me.use.android@gmail.com>
2014-11-13 16:51:13 -0700

    PHP working in widgets

--- functions.php
+++ functions.php
@@ -294,5 +294,19 @@ function rgps_modal_data( $atts, $item, $args ) {
 
 
 
+/* PHP working in widgets */
+add_filter('widget_text','rgps_execute_php',100);
+function rgps_execute_php($html){
+    if(strpos($html,"<"."?php")!==false){
+        ob_start();
+        eval("?".">".$html);
+        $html=ob_get_contents();
+        ob_end_clean();
+    }
+    return $html;
+}
+
+
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
 
